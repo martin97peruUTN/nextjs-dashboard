502fe32803a884ad557ed2155623da2311806832
Martin Perussini <mperussini@frsf.utn.edu.ar>
2024-04-26 14:50:15 -0300

    Chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -42,11 +42,16 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
 
     //Once the database has been updated, the /dashboard/invoices path will be revalidated, 
     //and fresh data will be fetched from the server.
@@ -76,11 +81,21 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+    } catch (error) {
+        return { message: 'Database Error: Failed to Update Invoice.' };
+    }
+
+    /**
+     * Note how redirect is being called outside of the try/catch block. This is because redirect works by 
+     * throwing an error, which would be caught by the catch block. To avoid this, you can call redirect 
+     * after try/catch. redirect would only be reachable if try is successful.
+     */
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -89,6 +104,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+
+    //Test
+    throw new Error('Failed to Delete Invoice');
+
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Deleted Invoice.' };
+    } catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
 }
